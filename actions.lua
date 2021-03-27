a66dc40845b3462b4bfe42969b95da8443b6d2ac
Ferronn <ferronn@ferronn.dev>
2021-03-27 17:58:07 -0500

    progress on LAB

--- actions.lua
+++ actions.lua
@@ -1,4 +1,6 @@
 local addonName = ...
 local LAB10 = LibStub('LibActionButton-1.0')
-local header = CreateFrame('Frame', nil, nil, 'SecureHandlerStateTemplate')
-LAB10:CreateButton(1, addonName .. 'ActionButton1', header)
+local header = CreateFrame('Frame', nil, UIParent, 'SecureHandlerStateTemplate')
+local button = LAB10:CreateButton(1, addonName .. 'ActionButton1', header)
+button:SetState(0, 'spell', 9472)
+button:DisableDragNDrop()
