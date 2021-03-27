f02bf10d26ae686888acdfdf489b2394a1e6e7ac
Ferronn <ferronn@ferronn.dev>
2021-03-27 18:03:23 -0500

    working button in the corner YAY

--- actions.lua
+++ actions.lua
@@ -1,6 +1,8 @@
 local addonName = ...
 local LAB10 = LibStub('LibActionButton-1.0')
 local header = CreateFrame('Frame', nil, UIParent, 'SecureHandlerStateTemplate')
+header:SetAllPoints()
 local button = LAB10:CreateButton(1, addonName .. 'ActionButton1', header)
 button:SetState(0, 'spell', 9472)
-button:DisableDragNDrop()
+button:SetPoint('BOTTOMRIGHT')
+button:DisableDragNDrop(true)
