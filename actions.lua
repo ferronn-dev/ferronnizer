eae55b398278858707622582fc560ac1ba73fe17
Ferronn <ferronn@ferronn.dev>
2021-03-27 20:00:30 -0500

    set state on login

--- actions.lua
+++ actions.lua
@@ -1,8 +1,13 @@
-local addonName = ...
+local addonName, G = ...
 local LAB10 = LibStub('LibActionButton-1.0')
 local header = CreateFrame('Frame', nil, UIParent, 'SecureHandlerStateTemplate')
 header:SetAllPoints()
 local button = LAB10:CreateButton(1, addonName .. 'ActionButton1', header)
-button:SetState(0, 'spell', 9472)
 button:SetPoint('BOTTOMRIGHT')
 button:DisableDragNDrop(true)
+
+G.Eventer({
+  PLAYER_LOGIN = function()
+    button:SetState(0, 'spell', 9472)
+  end,
+})
