6e062a126d23e7ee2757ff1ee351dbc0e3fcbf4d
Ferronn <ferronn@ferronn.dev>
2021-03-28 14:00:37 -0500

    48 buttons

--- actions.lua
+++ actions.lua
@@ -1,13 +1,35 @@
 local addonName, G = ...
-local LAB10 = LibStub('LibActionButton-1.0')
-local header = CreateFrame('Frame', nil, UIParent, 'SecureHandlerStateTemplate')
-header:SetAllPoints()
-local button = LAB10:CreateButton(1, addonName .. 'ActionButton1', header)
-button:SetPoint('BOTTOMRIGHT')
-button:DisableDragNDrop(true)
+
+local buttons = (function()
+  local LAB10 = LibStub('LibActionButton-1.0')
+  local prefix = addonName .. 'ActionButton'
+  local header = CreateFrame('Frame', prefix .. 'Header', UIParent, 'SecureHandlerStateTemplate')
+  local buttons = {}
+  for i = 1, 49 do
+    local button = LAB10:CreateButton(i, prefix .. i, header)
+    button:DisableDragNDrop(true)
+    table.insert(buttons, button)
+  end
+  for i, button in ipairs(buttons) do
+    if i <= 36 then
+      button:SetPoint('BOTTOM', buttons[i + 12], 'TOP')
+    end
+    if (i - 1) % 12 < 5 then
+      button:SetPoint('RIGHT', buttons[i + 1], 'LEFT')
+    elseif (i - 1) % 12 > 6 then
+      button:SetPoint('LEFT', buttons[i - 1], 'RIGHT')
+    end
+  end
+  buttons[42]:SetPoint('BOTTOMRIGHT', UIParent, 'BOTTOM')
+  buttons[43]:SetPoint('BOTTOMLEFT', UIParent, 'BOTTOM')
+  return buttons
+end)()
 
 G.Eventer({
   PLAYER_LOGIN = function()
-    button:SetState(0, 'spell', 9472)
+    G.ReparentFrame(MainMenuBar)
+    for _, button in ipairs(buttons) do
+      button:SetState(0, 'spell', 9472)
+    end
   end,
 })
--- actions_spec.lua
+++ actions_spec.lua
@@ -1,5 +1,6 @@
 describe('Actions', function()
   it('makes a button', function()
+    wow.state:SendEvent('PLAYER_LOGIN')
     local button = wow.state.frames['mooActionButton1']
     button:Click()
   end)
