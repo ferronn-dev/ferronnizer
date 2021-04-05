300c471052084f3278a7671f853abeafeb52322c
Ferronn <ferronn@ferronn.dev>
2021-04-05 21:58:15 +0000

    test drink button with drinkdb

--- drink_spec.lua
+++ drink_spec.lua
@@ -1,9 +1,15 @@
-describe('drink', function()
+describe('drink button', function()
   it('defaults to thirsty emote', function()
     local button = wow.state.frames['DrinkButton']
     button:Click()
     assert.same('macro', button:GetAttribute('type'))
     assert.same('', button:GetAttribute('macrotext'))
     assert.same({{ macro = '/thirsty [@none]' }}, wow.state.commands)
+  end)
+  it('drinks', function()
+    wow.state.inventory[3772] = 10
+    wow.state.inventory[2288] = 5
+    wow.env.DrinkButton:Click()
+    assert.same({{ macro = '/use item:3772' }}, wow.state.commands)
   end)
 end)
