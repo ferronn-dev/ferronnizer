d59449917a5e8a25c701029fc8c31733eb02d494
Ferronn <ferronn@ferronn.dev>
2021-04-07 22:41:07 +0000

    better save test

--- save_spec.lua
+++ save_spec.lua
@@ -1,7 +1,19 @@
 describe('save', function()
-  it('does not crash through event sequence', function()
+  it('saves something', function()
     wow.state:SendEvent('PLAYER_LOGIN')
     wow.state:SendEvent('PLAYER_ENTERING_WORLD')
     wow.state:SendEvent('PLAYER_LOGOUT')
+    local expected = {
+      bags = {},
+      class = 'PALADIN',
+      equipment = {},
+      level = 42,
+      name = 'Kewhand',
+      race = 'Human',
+      realm = 'Realm',
+      talents = {},
+      url = 'https://classic.wowhead.com/gear-planner/paladin/human/AioA',
+    }
+    assert.same(expected, wow.env.FerronnizerPlayerData)
   end)
 end)
