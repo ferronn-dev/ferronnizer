67915cde97d12b69db11344fbdab892e9a4a4ca7
Ferronn <ferronn@ferronn.dev>
2021-04-07 21:58:30 -0500

    start using my own action definition

--- actions.lua
+++ actions.lua
@@ -1,68 +1,81 @@
 local addonName, G = ...
 
---[[
-local characters = {
-  ['Shydove-Westfall'] = {
-    [1] = {
-      actionText = 'GH4',
-      mouseover = true,
-      spell = 'Greater Heal(Rank 4)',
+local actions = (function()
+  local characters = {
+    ['Shydove-Westfall'] = {
+      [1] = {
+        actionText = 'GH4',
+        mouseover = true,
+        spell = 'Greater Heal(Rank 4)',
+      },
+      [2] = {
+        actionText = 'GH2',
+        mouseover = true,
+        spell = 'Greater Heal(Rank 2)',
+      },
+      [3] = {
+        actionText = 'H4',
+        mouseover = true,
+        spell = 'Heal(Rank 4)',
+      },
+      [4] = {
+        actionText = 'Stop',
+        macro = '/stopcasting',
+        tooltip = 'Stop Casting',
+      },
+      [13] = {
+        actionText = 'FH7',
+        mouseover = true,
+        spell = 'Flash Heal(Rank 7)',
+      },
+      [14] = {
+        actionText = 'FH3',
+        mouseover = true,
+        spell = 'Flash Heal(Rank 3)',
+      },
+      [15] = {
+        actionText = 'FH2',
+        mouseover = true,
+        spell = 'Flash Heal(Rank 2)',
+      },
+      [16] = {
+        actionText = 'Stop',
+        macro = '/stopcasting',
+        tooltip = 'Stop Casting',
+      },
     },
-    [2] = {
-      actionText = 'GH2',
-      mouseover = true,
-      spell = 'Greater Heal(Rank 2)',
-    },
-    [3] = {
-      actionText = 'H4',
-      mouseover = true,
-      spell = 'Heal(Rank 4)',
-    },
-    [4] = {
-      actionText = 'Stop',
-      macro = '/stopcasting',
-      tooltip = 'Stop Casting',
-    },
-    [13] = {
-      actionText = 'FH7',
-      mouseover = true,
-      spell = 'Flash Heal(Rank 7)',
-    },
-    [14] = {
-      actionText = 'FH3',
-      mouseover = true,
-      spell = 'Flash Heal(Rank 3)',
-    },
-    [15] = {
-      actionText = 'FH2',
-      mouseover = true,
-      spell = 'Flash Heal(Rank 2)',
-    },
-    [16] = {
-      actionText = 'Stop',
-      macro = '/stopcasting',
-      tooltip = 'Stop Casting',
-    },
-  },
-}
-]]--
+  }
+  local charName = UnitName('player')..'-'..GetRealmName()
+  return characters[charName] or {}
+end)()
 
 local buttonMixin = {
-  GetActionText = nil,
+  GetActionText = function(self)
+    local action = actions[self._state_action]
+    return action and action.actionText or ""
+  end,
   GetCharges = nil,
   GetCooldown = nil,
   GetCount = nil,
   GetLossOfControlCooldown = nil,
   GetSpellId = nil,
-  GetTexture = nil,
-  HasAction = nil,
+  GetTexture = function(self)
+    local action = actions[self._state_action]
+    return action and action.spell and _G.GetSpellTexture(action.spell)
+  end,
+  HasAction = function()
+    return true
+  end,
   IsAttack = nil,
   IsAutoRepeat = nil,
   IsConsumableOrStackable = nil,
   IsCurrentlyActive = nil,
   IsEquipped = nil,
   IsUnitInRange = nil,
-  IsUsable = nil,
+  IsUsable = function(self)
+    local action = actions[self._state_action]
+    return action and action.spell and IsUsableSpell(action.spell)
+  end,
   SetTooltip = nil,
 }
 
@@ -75,7 +88,6 @@ local buttons = (function()
     local button = LAB10:CreateButton(i, prefix .. i, header)
     button.icon:SetTexCoord(0.07, 0.93, 0.07, 0.93)
     button:DisableDragNDrop(true)
-    Mixin(button, buttonMixin)
     table.insert(buttons, button)
   end
   for i, button in ipairs(buttons) do
@@ -107,7 +119,20 @@ G.Eventer({
   PLAYER_LOGIN = function()
     G.ReparentFrame(MainMenuBar)
     for i, button in ipairs(buttons) do
-      button:SetState(0, 'action', i)
+      local action = actions[i]
+      if action then
+        Mixin(button, buttonMixin)
+        button:SetState(0, nil, i)
+        if action.spell then
+          button:SetAttribute('type', 'macro')
+          button:SetAttribute('macrotext', '/dismount\n/stand\n/cast '..action.spell)
+        elseif action.macro then
+          button:SetAttribute('type', 'macro')
+          button:SetAttribute('macrotext', action.macro)
+        end
+      else
+        button:SetState(0, 'action', i)
+      end
     end
   end,
 })
