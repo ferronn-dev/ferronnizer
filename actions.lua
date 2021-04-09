beaf72033bc0fe239c1693b740bf3cad4ef1c857
Ferronn <ferronn@ferronn.dev>
2021-04-09 10:33:36 -0400

    kewhand row 1

--- actions.lua
+++ actions.lua
@@ -4,6 +4,48 @@ local libCount = LibStub('LibClassicSpellActionCount-1.0')
 
 local actions = (function()
   local characters = {
+    ['Kewhand-Westfall'] = {
+      [1] = {
+        spell = 'Judgement'
+      },
+      [2] = {
+        mouseover = true,
+        spell = 'Holy Light(Rank 8)'
+      },
+      [3] = {
+        mouseover = true,
+        spell = 'Flash of Light(Rank 6)'
+      },
+      [4] = {
+        mouseover = true,
+        spell = 'Holy Shock(Rank 3)'
+      },
+      [5] = {
+        spell = 'Hammer of Justice'
+      },
+      [6] = {
+        spell = 'Consecration'
+      },
+      [7] = {
+        mouseover = true,
+        spell = 'Cleanse'
+      },
+      [8] = {
+        spell = 'Divine Favor'
+      },
+      [9] = {
+        spell = 'Exorcism'
+      },
+      [10] = {
+        spell = 'Hammer of Wrath'
+      },
+      [11] = {
+        spell = 'Holy Wrath'
+      },
+      [12] = {
+        spell = 'Turn Undead'
+      },
+    },
     ['Shydove-Westfall'] = {
       [1] = {
         actionText = 'GH4',
