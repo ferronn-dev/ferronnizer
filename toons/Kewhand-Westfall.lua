9058636916a6884166a85188e2740b75dd17f03b
Ferronn <ferronn@ferronn.dev>
2021-04-14 13:33:43 +0000

    move toons into separate files

---
+++ toons/Kewhand-Westfall.lua
@@ -0,0 +1,151 @@
+local _, G = ...
+G.Characters = G.Characters or {}
+G.Characters['Kewhand-Westfall'] = {
+  [1] = {
+    spell = 'Judgement',
+  },
+  [2] = {
+    mouseover = true,
+    spell = 'Holy Light(Rank 8)',
+  },
+  [3] = {
+    mouseover = true,
+    spell = 'Flash of Light(Rank 6)',
+  },
+  [4] = {
+    mouseover = true,
+    spell = 'Holy Shock(Rank 3)',
+  },
+  [5] = {
+    spell = 'Hammer of Justice',
+  },
+  [6] = {
+    spell = 'Consecration',
+  },
+  [7] = {
+    mouseover = true,
+    spell = 'Cleanse',
+  },
+  [8] = {
+    spell = 'Divine Favor',
+  },
+  [9] = {
+    spell = 'Exorcism',
+  },
+  [10] = {
+    spell = 'Hammer of Wrath',
+  },
+  [11] = {
+    spell = 'Holy Wrath',
+  },
+  [12] = {
+    spell = 'Turn Undead',
+  },
+  [13] = {
+    spell = 'Devotion Aura',
+  },
+  [14] = {
+    spell = 'Retribution Aura',
+  },
+  [15] = {
+    spell = 'Concentration Aura',
+  },
+  [16] = {
+    spell = 'Frost Resistance Aura',
+  },
+  [17] = {
+    spell = 'Fire Resistance Aura',
+  },
+  [18] = {
+    spell = 'Shadow Resistance Aura',
+  },
+  [19] = {
+    spell = 'Blessing of Might',
+  },
+  [20] = {
+    spell = 'Righteous Fury',
+  },
+  [21] = {
+    spell = 'Redemption',
+  },
+  [22] = {
+    spell = 'Greater Blessing of Light',
+  },
+  [23] = {
+    spell = 'Greater Blessing of Salvation',
+  },
+  [24] = {
+    spell = 'Greater Blessing of Might',
+  },
+  [25] = {
+    spell = 'Seal of Righteousness',
+  },
+  [26] = {
+    spell = 'Seal of Wisdom',
+  },
+  [27] = {
+    spell = 'Seal of the Crusader',
+  },
+  [28] = {
+    spell = 'Seal of Justice',
+  },
+  [29] = {
+    spell = 'Seal of Light',
+  },
+  [30] = {
+    spell = 'Divine Shield',
+  },
+  [31] = {
+    mouseover = true,
+    spell = 'Blessing of Freedom',
+  },
+  [32] = {
+    mouseover = true,
+    spell = 'Blessing of Protection',
+  },
+  [33] = {
+    mouseover = true,
+    spell = 'Blessing of Sacrifice',
+  },
+  [34] = {
+    spell = 'Blessing of Salvation',
+  },
+  [35] = {
+    spell = 'Blessing of Wisdom',
+  },
+  [36] = {
+    spell = 'Blessing of Light',
+  },
+  [39] = {
+    spell = 'Smelting',
+  },
+  [41] = {
+    mouseover = true,
+    spell = 'Lay on Hands',
+  },
+  [42] = {
+    spell = 'Divine Intervention',
+  },
+  [43] = {
+    macro = '/click MountButton',
+    texture = 132261,
+    tooltip = 'Mount',
+  },
+  [44] = {
+    macro = '/click FollowButton',
+    texture = 132096,
+    tooltip = 'Follow Me',
+  },
+  [45] = {
+    item = 6948,
+  },
+  [46] = {
+    spell = 'Blessing of Kings',
+  },
+  [47] = {
+    spell = 'Greater Blessing of Kings',
+  },
+  [48] = {
+    spell = 'Greater Blessing of Wisdom',
+  },
+}
---
+++ toons/Shydove-Westfall.lua
@@ -0,0 +1,174 @@
+local _, G = ...
+G.Characters = G.Characters or {}
+G.Characters['Shydove-Westfall'] = {
+  [1] = {
+    actionText = 'GH4',
+    mouseover = true,
+    spell = 'Greater Heal(Rank 4)',
+  },
+  [2] = {
+    actionText = 'GH2',
+    mouseover = true,
+    spell = 'Greater Heal(Rank 2)',
+  },
+  [3] = {
+    actionText = 'H4',
+    mouseover = true,
+    spell = 'Heal(Rank 4)',
+  },
+  [4] = {
+    actionText = 'Stop',
+    macro = '/stopcasting',
+    texture = 135768,
+    tooltip = 'Stop Casting',
+  },
+  [5] = {
+    mouseover = true,
+    spell = 'Renew',
+  },
+  [6] = {
+    spell = 'Holy Nova',
+  },
+  [7] = {
+    mouseover = true,
+    spell = 'Power Word: Shield',
+  },
+  [8] = {
+    spell = '!Shoot',
+  },
+  [9] = {
+    spell = 'Smite',
+  },
+  [10] = {
+    spell = 'Shadow Word: Pain',
+  },
+  [11] = {
+    spell = 'Mind Blast',
+  },
+  [12] = {
+    spell = 'Psychic Scream',
+  },
+  [13] = {
+    actionText = 'FH7',
+    mouseover = true,
+    spell = 'Flash Heal(Rank 7)',
+  },
+  [14] = {
+    actionText = 'FH3',
+    mouseover = true,
+    spell = 'Flash Heal(Rank 3)',
+  },
+  [15] = {
+    actionText = 'FH2',
+    mouseover = true,
+    spell = 'Flash Heal(Rank 2)',
+  },
+  [16] = {
+    actionText = 'Stop',
+    macro = '/stopcasting',
+    texture = 135768,
+    tooltip = 'Stop Casting',
+  },
+  [17] = {
+    spell = 'Inner Focus',
+  },
+  [18] = {
+    spell = 'Prayer of Healing',
+  },
+  [19] = {
+    item = 19950,
+  },
+  [20] = {
+    item = 19990,
+  },
+  [21] = {
+    mouseover = true,
+    spell = 'Power Infusion',
+  },
+  [22] = {
+    mouseover = true,
+    spell = 'Shadow Protection',
+  },
+  [23] = {
+    mouseover = true,
+    spell = 'Power Word: Fortitude',
+  },
+  [24] = {
+    mouseover = true,
+    spell = 'Divine Spirit',
+  },
+  [25] = {
+    mouseover = true,
+    spell = 'Dispel Magic',
+  },
+  [26] = {
+    mouseover = true,
+    spell = 'Abolish Disease',
+  },
+  [27] = {
+    spell = 'Fade',
+  },
+  [28] = {
+    spell = 'Desperate Prayer',
+  },
+  [29] = {
+    spell = 'Inner Fire',
+  },
+  [30] = {
+    spell = 'Shackle Undead',
+  },
+  [31] = {
+    spell = 'Perception',
+  },
+  [32] = {
+    spell = 'Holy Fire',
+  },
+  [33] = {
+    spell = 'Resurrection',
+  },
+  [34] = {
+    spell = 'Prayer of Shadow Protection',
+  },
+  [35] = {
+    spell = 'Prayer of Fortitude',
+  },
+  [36] = {
+    spell = 'Prayer of Spirit',
+  },
+  [37] = {
+    spell = 'Fishing',
+  },
+  [38] = {
+    spell = 'Tailoring',
+  },
+  [39] = {
+    spell = 'Cooking',
+  },
+  [40] = {
+    spell = 'First Aid',
+  },
+  [41] = {
+    spell = 'Mind Control',
+  },
+  [42] = {
+    spell = 'Find Herbs',
+  },
+  [43] = {
+    item = 18778,
+  },
+  [44] = {
+    item = 13446,
+  },
+  [45] = {
+    item = 13444,
+  },
+  [46] = {
+    item = 8079,
+  },
+  [47] = {
+    spell = 'Mana Burn',
+  },
+  [48] = {
+    spell = 'Mind Soothe',
+  },
+}
