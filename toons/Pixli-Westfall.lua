fb3aa1a073078965df9598c270f44ad76c63e790
Ferronn <ferronn@ferronn.dev>
2021-04-14 16:54:52 -0500

    start Pixli bindings

---
+++ toons/Pixli-Westfall.lua
@@ -0,0 +1,34 @@
+local _, G = ...
+G.Characters = G.Characters or {}
+G.Characters['Pixli-Westfall'] = {
+  [1] = {
+    spell = 'Frostbolt',
+  },
+  [2] = {
+    spell = 'Fireball',
+  },
+  [3] = {
+    spell = 'Fire Blast',
+  },
+  [4] = {
+    spell = 'Arcane Missiles',
+  },
+  [5] = {
+    spell = 'Polymorph',
+  },
+  [18] = {
+    spell = 'Escape Artist',
+  },
+  [33] = {
+    spell = 'Conjure Food',
+  },
+  [34] = {
+    macro = '/click PartyBuffButton',
+  },
+  [45] = {
+    item = 5349,
+  },
+  [46] = {
+    drink = true,
+  },
+}
