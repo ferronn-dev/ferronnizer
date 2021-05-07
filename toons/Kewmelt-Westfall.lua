f973fecac8679611697f800c0740c67fb5fcd548
Ferronn <ferronn@ferronn.dev>
2021-05-06 20:59:53 -0500

    add kewmelt for now

---
+++ toons/Kewmelt-Westfall.lua
@@ -0,0 +1,107 @@
+local addonName, G = ...
+G.Characters = G.Characters or {}
+G.Characters['Kewmelt-Westfall'] = {
+  [1] = {
+    spell = 'Frostbolt',
+  },
+  [2] = {
+    spell = 'Fireball',
+  },
+  [3] = {
+    spell = 'Fire Blast',
+  },
+  [4] = {
+    spell = 'Arcane Missiles',
+  },
+  [5] = {
+    spell = 'Polymorph',
+  },
+  [6] = {
+    spell = 'Frost Nova',
+  },
+  [7] = {
+    spell = 'Arcane Explosion',
+  },
+  [8] = {
+    spell = '!Shoot',
+  },
+  [9] = {
+    spell = 'Flamestrike',
+  },
+  [10] = {
+    spell = 'Blizzard',
+  },
+  [11] = {
+    spell = 'Scorch',
+  },
+  [12] = {
+    spell = 'Cone of Cold',
+  },
+  [13] = {
+    mouseover = true,
+    spell = 'Remove Lesser Curse',
+  },
+  [14] = {
+    spell = 'Blink',
+  },
+  [15] = {
+    mouseover = true,
+    spell = 'Amplify Magic',
+  },
+  [16] = {
+    mouseover = true,
+    spell = 'Dampen Magic',
+  },
+  [17] = {
+    spell = 'Detect Magic',
+  },
+  [18] = {
+    spell = 'Escape Artist',
+  },
+  [19] = {
+    invslot = 13,
+  },
+  [20] = {
+    invslot = 14,
+  },
+  [21] = {
+    spell = 'Cold Snap',
+  },
+  [22] = {
+    spell = 'Evocation',
+  },
+  [23] = {
+    spell = 'Ice Barrier',
+  },
+  [24] = {
+    spell = 'Ice Block',
+  },
+  [25] = {
+    spell = 'Mana Shield',
+  },
+  [26] = {
+    spell = 'Fire Ward',
+  },
+  [27] = {
+    spell = 'Frost Ward',
+  },
+  [28] = {
+    spell = 'Counterspell',
+    stopcasting = true,
+  },
+  [34] = {
+    buff = true,
+  },
+  [36] = {
+    spell = 'Slow Fall',
+  },
+  [43] = {
+    mount = true,
+  },
+  [45] = {
+    eat = true,
+  },
+  [46] = {
+    drink = true,
+  },
+}
