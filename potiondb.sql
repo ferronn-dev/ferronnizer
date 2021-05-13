cffd3ff7fb737b969f1828c1f7031411d644b5bb
Ferronn <ferronn@ferronn.dev>
2021-05-12 20:47:55 -0500

    better health potion query

--- potiondb.sql
+++ potiondb.sql
@@ -1,8 +1,13 @@
-SELECT CAST(id AS INT64)
-FROM itemsparse s
-WHERE display_lang LIKE '%Healing Potion'
-ORDER BY CAST(itemlevel AS INT64) DESC;
+-- HealthPotionDB
+SELECT CAST(s.id AS INT64)
+FROM itemsparse s, itemeffect e, spelleffect z
+WHERE s.id = e.parentitemid
+  AND e.spellcategoryid = "4"
+  AND z.spellid = e.spellid
+  AND z.effect = "10"
+ORDER BY CAST(s.itemlevel AS INT64) DESC;
 
+-- ManaPotionDB
 SELECT CAST(s.id AS INT64)
 FROM itemsparse s, itemeffect e, spelleffect z
 WHERE s.id = e.parentitemid
