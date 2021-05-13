9d706ff5d617dd423fb31ec2371b8b2a5b7458cb
Ferronn <ferronn@ferronn.dev>
2021-05-12 20:41:37 -0500

    better mana potion query, fixes #44

--- potiondb.sql
+++ potiondb.sql
@@ -3,7 +3,10 @@ FROM itemsparse s
 WHERE display_lang LIKE '%Healing Potion'
 ORDER BY CAST(itemlevel AS INT64) DESC;
 
-SELECT CAST(id AS INT64)
-FROM itemsparse s
-WHERE display_lang LIKE '%Mana Potion'
-ORDER BY CAST(itemlevel AS INT64) DESC;
+SELECT CAST(s.id AS INT64)
+FROM itemsparse s, itemeffect e, spelleffect z
+WHERE s.id = e.parentitemid
+  AND e.spellcategoryid = "4"
+  AND z.spellid = e.spellid
+  AND z.effect = "30"
+ORDER BY CAST(s.itemlevel AS INT64) DESC;
