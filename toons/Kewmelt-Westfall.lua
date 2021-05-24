d08bf75cfc93ee6f7f29620edde959f3cf9cfdca
Ferronn <ferronn@ferronn.dev>
2021-05-24 20:13:59 +0000

    only set Detect Magic in classic

--- toons/Kewmelt-Westfall.lua
+++ toons/Kewmelt-Westfall.lua
@@ -52,9 +52,9 @@ G.Characters['Kewmelt-Westfall'] = {
     mouseover = true,
     spell = 'Dampen Magic',
   },
-  [17] = {
+  [17] = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and {
     spell = 'Detect Magic',
-  },
+  } or nil),
   [18] = {
     spell = 'Escape Artist',
   },
--- toons/Pixli-Westfall.lua
+++ toons/Pixli-Westfall.lua
@@ -49,9 +49,9 @@ G.Characters['Pixli-Westfall'] = {
     mouseover = true,
     spell = 'Dampen Magic',
   },
-  [17] = {
+  [17] = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and {
     spell = 'Detect Magic',
-  },
+  } or nil),
   [18] = {
     spell = 'Escape Artist',
   },
