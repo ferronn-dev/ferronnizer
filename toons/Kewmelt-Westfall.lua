f3b6d1084f84e668323dce69f0769b3dfde12afe
Ferronn <ferronn@ferronn.dev>
2021-06-04 20:01:19 +0000

    align kewmelt and pixli

--- toons/Kewmelt-Westfall.lua
+++ toons/Kewmelt-Westfall.lua
@@ -52,9 +52,9 @@ G.Characters['Kewmelt-Westfall'] = {
     mouseover = true,
     spell = 'Dampen Magic',
   },
-  [17] = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and {
-    spell = 'Detect Magic',
-  } or nil),
+  [17] = {
+    spell = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and 'Detect Magic' or 'Icy Veins',
+  },
   [18] = {
     spell = 'Escape Artist',
   },
