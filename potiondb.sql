bc1b5f842a81f596f8a7f2712d1048c5bbe73d35
Ferronn <ferronn@ferronn.dev>
2021-08-19 19:42:25 +0000

    add healthstones and mana gems to potiondb

--- potiondb.sql
+++ potiondb.sql
@@ -3,7 +3,7 @@ SELECT CAST(s.id AS INT64), CAST(s.requiredlevel AS INT64)
 FROM itemsparse s, itemeffect e, spelleffect z
 WHERE s.id = e.parentitemid
   AND e.spellid <> "41620"  -- Bottled Nethergon Vapor
-  AND e.spellcategoryid = "4"
+  AND e.spellcategoryid IN ("4", "1153")
   AND z.spellid = e.spellid
   AND z.effect = "10"
 ORDER BY CAST(s.itemlevel AS INT64) DESC;
@@ -13,7 +13,7 @@ SELECT CAST(s.id AS INT64), CAST(s.requiredlevel AS INT64)
 FROM itemsparse s, itemeffect e, spelleffect z
 WHERE s.id = e.parentitemid
   AND e.spellid <> "41618"  -- Bottled Nethergon Energy
-  AND e.spellcategoryid = "4"
+  AND e.spellcategoryid IN ("4", "1153")
   AND z.spellid = e.spellid
   AND z.effect = "30"
 ORDER BY CAST(s.itemlevel AS INT64) DESC;
