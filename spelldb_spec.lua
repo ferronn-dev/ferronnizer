295e6f32c20717861c6c419e8ad86b9e149530d2
Ferronn <ferronn@ferronn.dev>
2021-08-24 16:43:09 +0000

    add a test for #96

--- spelldb_spec.lua
+++ spelldb_spec.lua
@@ -27,4 +27,41 @@ describe('spell db', function()
     end
     assert.same(knownBad, seenBad)
   end)
+  it('does not duplicate spell ids', function()
+    -- TODO make this table empty instead
+    local expected = ({
+      [2] = {
+        ['Bite'] = true,
+        ['Claw'] = true,
+        ['Consecration'] = true,
+        ['Dash'] = true,
+        ['Dive'] = true,
+        ['Immolate'] = true,
+        ['Intimidation'] = true,
+        ['Screech'] = true,
+      },
+      [5] = {
+        ['Bite'] = true,
+        ['Claw'] = true,
+        ['Dash'] = true,
+        ['Dive'] = true,
+        ['Gore'] = true,
+        ['Greater Heal'] = true,
+        ['Immolate'] = true,
+        ['Intimidation'] = true,
+        ['Screech'] = true,
+      },
+    })[wow.env.WOW_PROJECT_ID]
+    local actual = {}
+    for name, ranks in pairs(wow.addon.SpellDB) do
+      local ids = {}
+      for _, rank in ipairs(ranks) do
+        if ids[rank[1]] then
+          actual[name] = true
+        end
+        ids[rank[1]] = true
+      end
+    end
+    assert.same(expected, actual)
+  end)
 end)
