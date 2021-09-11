ecf794e0908b10ec9f0b9e616c53b964b449c16e
Ferronn <ferronn@ferronn.dev>
2021-09-11 15:46:36 -0500

    introduce tbc warrior

---
+++ toons/TBC/Warrior.lua
@@ -0,0 +1,18 @@
+local _, G = ...
+G.AddClassActionSpec('TBC', 1, {
+  [1] = {
+    spell = 'Heroic Strike',
+  },
+  [34] = {
+    spell = 'Gift of the Naaru',
+  },
+  [43] = {
+    mount = true,
+  },
+  [45] = {
+    eat = true,
+  },
+  [46] = {
+    drink = true,
+  },
+})
