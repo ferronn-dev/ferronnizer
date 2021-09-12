a4f6a8b7ffcaff03298e5172e75bdf92934c2e20
Ferronn <ferronn@ferronn.dev>
2021-09-12 15:33:39 -0500

    use spelleffect instead of string in bandagedb

--- bandagedb.sql
+++ bandagedb.sql
@@ -1,6 +1,7 @@
 SELECT CAST(s.id AS INT64), CAST(s.requiredskillrank AS INT64)
-FROM itemsparse s, itemeffect e
+FROM itemsparse s, itemeffect e, spelleffect z
 WHERE s.id = e.parentitemid
-  AND s.display_lang LIKE '%Bandage%'
   AND e.spellcategoryid = '150'
+  AND e.spellid = z.spellid
+  AND z.effect = '6'
 ORDER BY CAST(s.requiredskillrank AS INT64) DESC;
