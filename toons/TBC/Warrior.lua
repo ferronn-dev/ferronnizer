da62cf51e8024f00c4f30aafae701f2f4c7598e3
Ferronn <ferronn@ferronn.dev>
2021-09-17 21:04:54 -0500

    lvl 12 warrior, also some stance info

--- toons/TBC/Warrior.lua
+++ toons/TBC/Warrior.lua
@@ -5,18 +5,26 @@ G.AddClassActionSpec('TBC', 1, {
   },
   [2] = {
     spell = 'Rend',
+    stances = {'Battle Stance', 'Defensive Stance'},
   },
   [3] = {
     spell = 'Sunder Armor',
   },
+  [4] = {
+    spell = 'Overpower',
+    stances = {'Battle Stance'},
+  },
   [5] = {
     spell = 'Charge',
+    stances = {'Battle Stance'},
   },
   [6] = {
     spell = 'Thunder Clap',
+    stances = {'Battle Stance', 'Defensive Stance'},
   },
   [7] = {
     spell = 'Hamstring',
+    stances = {'Battle Stance', 'Berserker Stance'},
   },
   [13] = {
     spell = 'Battle Shout',
@@ -30,8 +38,14 @@ G.AddClassActionSpec('TBC', 1, {
   [20] = {
     spell = 'Defensive Stance',
   },
+  [25] = {
+    shield = true,
+    spell = 'Shield Bash',
+    stances = {'Battle Stance', 'Defensive Stance'},
+  },
   [26] = {
     spell = 'Taunt',
+    stances = {'Defensive Stance'},
   },
   [33] = {
     spell = 'Gift of the Naaru',
