c80e030f1453bf5ffd40805523da73e6aedbef36
Ferronn <ferronn@ferronn.dev>
2021-09-19 14:12:17 -0500

    remove metadata that does nothing

--- toons/TBC/Warrior.lua
+++ toons/TBC/Warrior.lua
@@ -5,26 +5,21 @@ G.AddClassActionSpec('TBC', 1, {
   },
   [2] = {
     spell = 'Rend',
-    stances = {'Battle Stance', 'Defensive Stance'},
   },
   [3] = {
     spell = 'Sunder Armor',
   },
   [4] = {
     spell = 'Overpower',
-    stances = {'Battle Stance'},
   },
   [5] = {
     spell = 'Charge',
-    stances = {'Battle Stance'},
   },
   [6] = {
     spell = 'Thunder Clap',
-    stances = {'Battle Stance', 'Defensive Stance'},
   },
   [7] = {
     spell = 'Hamstring',
-    stances = {'Battle Stance', 'Berserker Stance'},
   },
   [8] = {
     spell = '!Shoot',
@@ -48,17 +43,13 @@ G.AddClassActionSpec('TBC', 1, {
     spell = 'Berserker Stance',
   },
   [25] = {
-    shield = true,
     spell = 'Shield Bash',
-    stances = {'Battle Stance', 'Defensive Stance'},
   },
   [26] = {
     spell = 'Taunt',
-    stances = {'Defensive Stance'},
   },
   [27] = {
     spell = 'Revenge',
-    stances = {'Defensive Stance'},
   },
   [33] = {
     spell = 'Gift of the Naaru',
