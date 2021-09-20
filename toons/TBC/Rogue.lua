d0aa49928a3c9877ec6b22b528d1f85a18486f0e
Ferronn <ferronn@ferronn.dev>
2021-09-19 22:00:41 -0500

    add stubs for remaining tbc classes

---
+++ toons/TBC/Rogue.lua
@@ -0,0 +1,21 @@
+local _, G = ...
+G.AddClassActionSpec('TBC', 4, {
+  [33] = {
+    racial = true,
+  },
+  [34] = {
+    buff = true,
+  },
+  [43] = {
+    mount = true,
+  },
+  [44] = {
+    bandage = true,
+  },
+  [45] = {
+    eat = true,
+  },
+  [46] = {
+    drink = true,
+  },
+})
