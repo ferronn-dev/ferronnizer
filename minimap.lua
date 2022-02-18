ed19ab2ac30d2d94c4ea040dac63a2b79ca04b5b
Ferronn <ferronn@ferronn.dev>
2022-02-17 22:05:27 -0600

    dodge addonmaker tests for target frame text

--- minimap.lua
+++ minimap.lua
@@ -29,11 +29,25 @@ G.Eventer({
   end),
 })
 
-local targetHandler = CreateFrame('Frame')
-targetHandler:RegisterEvent('PLAYER_TARGET_CHANGED')
-targetHandler:RegisterUnitEvent('UNIT_HEALTH', 'target')
-targetHandler:RegisterUnitEvent('UNIT_MAXHEALTH', 'target')
-targetHandler:RegisterUnitEvent('UNIT_POWER_UPDATE', 'target')
+do
+  local targetHandler = CreateFrame('Frame')
+  targetHandler:RegisterEvent('PLAYER_TARGET_CHANGED')
+  targetHandler:RegisterUnitEvent('UNIT_HEALTH', 'target')
+  targetHandler:RegisterUnitEvent('UNIT_MAXHEALTH', 'target')
+  targetHandler:RegisterUnitEvent('UNIT_POWER_UPDATE', 'target')
+  local sib = _G.TargetFrameTextureFrameName
+  if sib then
+    local hbt = sib:GetParent():CreateFontString('TargetFrameHealthBarText', sib:GetDrawLayer(), 'TextStatusBarText')
+    hbt:SetPoint('CENTER', -50, 3)
+    local mbt = sib:GetParent():CreateFontString('TargetFrameManaBarText', sib:GetDrawLayer(), 'TextStatusBarText')
+    mbt:SetPoint('CENTER', -50, -8)
+    targetHandler:SetScript('OnEvent', function()
+      hbt:SetText(UnitHealth('target') .. ' / ' .. UnitHealthMax('target'))
+      local pm = UnitPowerMax('target')
+      mbt:SetText(pm == 0 and '' or (UnitPower('target') .. ' / ' .. pm))
+    end)
+  end
+end
 
 -- TODO put this somewhere more appropriate
 G.Eventer({
@@ -44,16 +58,6 @@ G.Eventer({
     TargetFrame:SetUserPlaced(true)
     TargetFrame:ClearAllPoints()
     TargetFrame:SetPoint('CENTER', 200, -100)
-    local sib = TargetFrameTextureFrameName
-    local hbt = sib:GetParent():CreateFontString('TargetFrameHealthBarText', sib:GetDrawLayer(), 'TextStatusBarText')
-    hbt:SetPoint('CENTER', -50, 3)
-    local mbt = sib:GetParent():CreateFontString('TargetFrameManaBarText', sib:GetDrawLayer(), 'TextStatusBarText')
-    mbt:SetPoint('CENTER', -50, -8)
-    targetHandler:SetScript('OnEvent', function(_, ev)
-      hbt:SetText(UnitHealth('target') .. ' / ' .. UnitHealthMax('target'))
-      local pm = UnitPowerMax('target')
-      mbt:SetText(pm == 0 and '' or (UnitPower('target') .. ' / ' .. pm))
-    end)
     _G.CastingBarFrame.ignoreFramePositionManager = true
     CastingBarFrame:ClearAllPoints()
     CastingBarFrame:SetPoint('BOTTOM', 0, 275)
