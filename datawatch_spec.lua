2ea2f9d0127065f7938757db754175beb23f0a1b
Ferronn <ferronn@ferronn.dev>
2022-05-04 02:29:57 +0000

    enough to load

--- datawatch_spec.lua
+++ datawatch_spec.lua
@@ -1,11 +1,26 @@
 local function newDataWatch()
-  local env = {}
-  loadfile('datawatch.lua')('', env)
-  return env.DataWatch
+  local globalEnv = {
+    CreateFrame = function()
+      return {
+        RegisterEvent = function() end,
+        SetScript = function() end,
+      }
+    end,
+    pairs = pairs,
+    UnitLevel = function()
+      return 60
+    end,
+    UnitName = function()
+      return 'UnitName'
+    end,
+  }
+  local addonEnv = {}
+  setfenv(loadfile('datawatch.lua'), globalEnv)('', addonEnv)
+  return addonEnv.DataWatch
 end
 
 describe('datawatch', function()
   it('loads', function()
-    -- TODO newDataWatch()
+    newDataWatch()
   end)
 end)
