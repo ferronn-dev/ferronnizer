5ae37d38b9c2aa29f85065ad4ee5f3192b075151
Ferronn <ferronn@ferronn.dev>
2022-05-04 13:48:49 +0000

    works with no registrations

--- datawatch_spec.lua
+++ datawatch_spec.lua
@@ -1,11 +1,18 @@
 local function newDataWatch()
+  local scripts = {}
   local globalEnv = {
     CreateFrame = function()
       return {
         RegisterEvent = function() end,
-        SetScript = function() end,
+        SetScript = function(_, name, script)
+          scripts[name] = script
+        end,
       }
     end,
+    GameTime_GetTime = function()
+      return '4:20'
+    end,
+    ipairs = ipairs,
     pairs = pairs,
     UnitLevel = function()
       return 60
@@ -14,13 +21,16 @@ local function newDataWatch()
       return 'UnitName'
     end,
   }
+  globalEnv._G = globalEnv
   local addonEnv = {}
   setfenv(loadfile('datawatch.lua'), globalEnv)('', addonEnv)
-  return addonEnv.DataWatch
+  return addonEnv.DataWatch, scripts.OnEvent, scripts.OnUpdate
 end
 
 describe('datawatch', function()
-  it('loads', function()
-    newDataWatch()
+  it('works with no registrations', function()
+    local _, _, onUpdate = newDataWatch()
+    onUpdate()
+    onUpdate()
   end)
 end)
