6f72c76c76d85d2a84974d5e5b1e2d0142a9ee7e
Ferronn <ferronn@ferronn.dev>
2022-05-04 13:58:00 +0000

    game time test

--- datawatch_spec.lua
+++ datawatch_spec.lua
@@ -1,6 +1,10 @@
 local function newDataWatch()
+  local data = {
+    game_time = '4:20',
+  }
   local scripts = {}
   local globalEnv = {
+    assert = assert,
     CreateFrame = function()
       return {
         RegisterEvent = function() end,
@@ -10,10 +14,12 @@ local function newDataWatch()
       }
     end,
     GameTime_GetTime = function()
-      return '4:20'
+      return data.game_time
     end,
     ipairs = ipairs,
     pairs = pairs,
+    table = table,
+    tostring = tostring,
     UnitLevel = function()
       return 60
     end,
@@ -24,7 +30,7 @@ local function newDataWatch()
   globalEnv._G = globalEnv
   local addonEnv = {}
   setfenv(loadfile('datawatch.lua'), globalEnv)('', addonEnv)
-  return addonEnv.DataWatch, scripts.OnEvent, scripts.OnUpdate
+  return addonEnv.DataWatch, scripts.OnEvent, scripts.OnUpdate, data
 end
 
 describe('datawatch', function()
@@ -32,5 +38,18 @@ describe('datawatch', function()
     local _, _, onUpdate = newDataWatch()
     onUpdate()
     onUpdate()
+  end)
+  it('handles game_time', function()
+    local game_time = 'foo'
+    local watch, _, onUpdate, data = newDataWatch()
+    watch('game_time', function(x)
+      game_time = x
+    end)
+    assert.same('foo', game_time)
+    onUpdate()
+    assert.same('4:20', game_time)
+    data.game_time = '5:35'
+    onUpdate()
+    assert.same('5:35', game_time)
   end)
 end)
