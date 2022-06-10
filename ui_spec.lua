370dba9a3114ab9d1dab9279155ee5c99ffb73ee
Ferronn <ferronn@ferronn.dev>
2022-06-10 15:52:23 +0000

    simplify ui spec

--- ui_spec.lua
+++ ui_spec.lua
@@ -1,73 +1,7 @@
-local function loadUi()
-  local globalEnv
-  globalEnv = {
-    CreateFrame = function(_, name)
-      local f = {
-        CreateFontString = function()
-          return {
-            GetTextColor = function() end,
-            SetAllPoints = function() end,
-            SetFontObject = function() end,
-            SetHeight = function() end,
-            SetJustifyH = function() end,
-            SetJustifyV = function() end,
-            SetPoint = function() end,
-            SetSize = function() end,
-            SetTextColor = function() end,
-          }
-        end,
-        CreateTexture = function()
-          return {
-            SetAllPoints = function() end,
-            SetHeight = function() end,
-            SetPoint = function() end,
-            SetSize = function() end,
-            SetTexCoord = function() end,
-            SetTexture = function() end,
-            SetVertexColor = function() end,
-          }
-        end,
-        Hide = function() end,
-        RegisterEvent = function() end,
-        RegisterForClicks = function() end,
-        RegisterUnitEvent = function() end,
-        SetAllPoints = function() end,
-        SetAlpha = function() end,
-        SetAttribute = function() end,
-        SetPoint = function() end,
-        SetReverse = function() end,
-        SetScale = function() end,
-        SetScript = function() end,
-        SetSize = function() end,
-        SetStatusBarTexture = function() end,
-      }
-      if name then
-        globalEnv[name] = f
-      end
-      return f
-    end,
-    GetXPExhaustion = function() end,
-    ipairs = ipairs,
-    pairs = pairs,
-    RegisterUnitWatch = function() end,
-    select = select,
-    table = table,
-    type = type,
-    unpack = unpack,
-  }
-  globalEnv._G = globalEnv
-  local addonEnv = {
-    AddFrameWatch = function() end,
-    DataWatch = function() end,
-    Eventer = function() end,
-  }
-  setfenv(loadfile('ui.lua'), globalEnv)('', addonEnv)
-  return globalEnv.FerronnizerRoot
-end
-
 describe('ui', function()
   it('loads', function()
-    local root = loadUi()
+    local root = wow.env.FerronnizerRoot
+    assert.Not.Nil(root)
     assert.Not.Nil(root.Clock)
     assert.Not.Nil(root.Hidden)
   end)
