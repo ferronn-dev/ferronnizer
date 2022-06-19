55a9c9e9ed34033e22492746cefb21f3d8062939
Ferronn <ferronn@ferronn.dev>
2022-06-19 03:34:55 +0000

    ace config based console command

--- ace.lua
+++ ace.lua
@@ -1,23 +1,35 @@
-local toggleRaidFrames = (function()
-  local root = _G.FerronnizerRoot
-  local frames = {
-    [CompactRaidFrameManager] = UIParent,
-    [root.Party1] = root,
-    [root.Party2] = root,
-    [root.Party3] = root,
-    [root.Party4] = root,
-  }
-  local hidden = root.Hidden
-  return function()
-    for k, v in pairs(frames) do
-      k:SetParent(k:GetParent() == v and hidden or v)
-    end
-  end
-end)()
+local addonName = ...
 
-local ace = LibStub('AceAddon-3.0'):NewAddon('Ferronnizer', 'AceConsole-3.0')
-ace:RegisterChatCommand('ferronnizer', function(str)
-  if str == 'frames' then
-    toggleRaidFrames()
-  end
+local ace = LibStub('AceAddon-3.0'):NewAddon(addonName, 'AceConsole-3.0')
+LibStub('AceConfig-3.0'):RegisterOptionsTable(addonName, {
+  type = 'group',
+  args = {
+    frames = (function()
+      local state = true
+      return {
+        name = 'Frames',
+        desc = 'Toggles Blizzard raid frames',
+        type = 'toggle',
+        get = function()
+          return state
+        end,
+        set = (function()
+          local root = _G.FerronnizerRoot
+          local hidden = root.Hidden
+          return function(_, value)
+            state = value
+            CompactRaidFrameManager:SetParent(state and hidden or UIParent)
+            for i = 1, 4 do
+              root['Party' .. i]:SetParent(state and root or hidden)
+            end
+          end
+        end)(),
+      }
+    end)(),
+  },
+})
+local slash = 'ferronnizer'
+local handleCommand = LibStub('AceConfigCmd-3.0').HandleCommand
+ace:RegisterChatCommand(slash, function(input)
+  handleCommand(ace, slash, addonName, input)
 end)
