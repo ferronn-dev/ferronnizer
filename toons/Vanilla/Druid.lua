1299f993b59fa8691801868fb57f8ac658fd3749
Ferronn <ferronn@ferronn.dev>
2022-08-04 20:45:38 +0000

    start populating vanilla druid

--- toons/Vanilla/Druid.lua
+++ toons/Vanilla/Druid.lua
@@ -1,19 +1,58 @@
 local _, G = ...
 G.AddClassActionSpec('Vanilla', 11, {
   [1] = {
-    spell = 'Wrath',
+    shapeshift = { 'Maul', '', 'Claw' },
   },
   [2] = {
-    spell = 'Moonfire',
+    shapeshift = { 'Swipe', '', 'Shred' },
+  },
+  [3] = {
+    shapeshift = { 'Bash', '', 'Rake' },
+  },
+  [4] = {
+    shapeshift = { 'Demoralizing Roar', '', 'Ferocious Bite' },
+  },
+  [5] = {
+    shapeshift = { 'Feral Charge', '', 'Prowl' },
+  },
+  [6] = {
+    shapeshift = { 'Enrage', '', 'Ravage' },
+  },
+  [7] = {
+    shapeshift = { 'Challenging Roar', '', 'Pounce' },
+  },
+  [8] = {
+    shapeshift = { 'Frenzied Regeneration', '', 'Rip' },
+  },
+  [9] = {
+    shapeshift = { 'Growl', 'Dash' },
+  },
+  [10] = {
+    spell = 'Cower',
+  },
+  [11] = {
+    spell = 'Tiger\'s Fury',
   },
   [12] = {
-    spell = 'Entangling Roots',
+    shapeshift = { 'Faerie Fire (Feral)', '', 'Faerie Fire (Feral)' },
   },
   [13] = {
     spell = 'Healing Touch',
   },
   [14] = {
     spell = 'Rejuvenation',
+  },
+  [25] = {
+    spells = { 'Dire Bear Form', 'Bear Form' },
+  },
+  [26] = {
+    spell = 'Cat Form',
+  },
+  [27] = {
+    spell = 'Travel Form',
+  },
+  [28] = {
+    spell = 'Aquatic Form',
   },
   [34] = {
     buff = true,
