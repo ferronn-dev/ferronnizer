ba7ecae171971886401560502609ccaf33ce2706
Ferronn <ferronn@ferronn.dev>
2024-09-28 17:21:19 -0500

    fix minimap

--- minimap.lua
+++ minimap.lua
@@ -1,9 +1,5 @@
 local _, G = ...
 
-local function ifClassic(x)
-  return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and x or nil
-end
-
 G.Eventer({
   PLAYER_LOGIN = function()
     G.ReparentFrame(MinimapBackdrop)
@@ -13,18 +9,17 @@ G.Eventer({
     Minimap:SetMaskTexture('Interface\\Buttons\\WHITE8X8')
     Minimap:SetScale(0.75)
     Minimap:SetZoom(0)
-    Minimap:SetPoint('TOP', UIParent, 'CENTER', 0, -200)
-    if MiniMapTracking then
-      MiniMapTracking:SetParent(Minimap)
-      MiniMapTracking:ClearAllPoints()
-      MiniMapTracking:SetPoint('TOPLEFT', Minimap, 'TOPLEFT')
-    end
+    Minimap:ClearAllPoints()
+    Minimap:SetPoint('BOTTOM', UIParent, 'BOTTOM', 0, 220)
+    MiniMapTracking:SetParent(Minimap)
+    MiniMapTracking:ClearAllPoints()
+    MiniMapTracking:SetPoint('TOPLEFT', Minimap, 'TOPLEFT')
   end,
-  PLAYER_ENTERING_WORLD = ifClassic(function()
+  PLAYER_ENTERING_WORLD = function()
     local t = GetTrackingTexture()
     if t then
       MiniMapTrackingIcon:SetTexture(t)
-      MiniMapTrackingFrame:Show()
+      MiniMapTracking:Show()
     end
-  end),
+  end,
 })
